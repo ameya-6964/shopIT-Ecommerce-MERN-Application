a2b306b701582e412e249b46b36206d50593100e
ameya-6964 <belvalkar7@gmail.com>
2023-06-22 22:12:37 +0530

    Changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -214,7 +214,7 @@ exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
 exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.query.productId);
 
-  // console.log(product);
+  console.log(product);
 
   const reviews = product.reviews.filter(
     (review) => review._id.toString() !== req.query.id.toString()
